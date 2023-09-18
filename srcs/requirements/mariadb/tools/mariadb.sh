14a37f25f98c169d4145e3c255478dfda60bdfb4
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-18 22:45:38 +0100

    update mariadb script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-while true; do
-    echo "Script Work Successfully"
-    sleep 2
-done
+service mysql start;
+
+	# Create database named ${SQL_DATABASE}
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+	# Create user with password
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY \`${SQL_PASSWORD}\`;"
+
+	#Grant all database privileges to sql user
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY \`${SQL_PASSWORD}\`;"
+
+	# Change the password for the root user in MySQL
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+#
+	# mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}'); FLUSH PRIVILEGES;"
+#
+
+	#Refresh all
+mysql -e "FLUSH PRIVILEGES;"
+
+	#Turning off MySQL
+mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+
+	#start the MySQL server in a safe mode 
+exec mysqld_safe
