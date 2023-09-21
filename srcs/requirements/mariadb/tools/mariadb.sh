f9972b2942859536310813c54ba441071d206d80
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-21 15:29:50 +0100

    update

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
-service mysql start;
+# Start the MySQL service
+service mysql start
 
-	# Create database named ${SQL_DATABASE}
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+# Create database
+mysql -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
 
-	# Create user with password
-mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY \`${SQL_PASSWORD}\`;"
+# Create user with password
+mysql -e "CREATE USER IF NOT EXISTS \`$SQL_USER\`@'localhost' IDENTIFIED BY '$SQL_PASSWORD';"
 
-	#Grant all database privileges to sql user
-mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY \`${SQL_PASSWORD}\`;"
+# Grant all database privileges to the SQL user
+mysql -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
-	# Change the password for the root user in MySQL
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+# Change the password for the root user in MySQL
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$SQL_ROOT_PASSWORD';"
 
-#
-	# mysql -e "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('${SQL_ROOT_PASSWORD}'); FLUSH PRIVILEGES;"
-#
-
-	#Refresh all
+# Flush privileges
 mysql -e "FLUSH PRIVILEGES;"
 
-	#Turning off MySQL
-mysqladmin -u root -p $SQL_ROOT_PASSWORD shutdown
+# Shutdown MySQL
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-	#start the MySQL server in a safe mode 
+# Start the MySQL server in safe mode
 exec mysqld_safe
