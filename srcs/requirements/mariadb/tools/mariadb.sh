201d90745c5b864fd627949d7ada2ca3f7bcc3ff
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-22 13:42:20 +0100

    update

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -7,7 +7,7 @@ service mysql start
 mysql -e "CREATE DATABASE IF NOT EXISTS \`$SQL_DATABASE\`;"
 
 # Create user with password
-mysql -e "CREATE USER IF NOT EXISTS \`$SQL_USER\`@'localhost' IDENTIFIED BY '$SQL_PASSWORD';"
+mysql -e "CREATE USER IF NOT EXISTS \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';"
 
 # Grant all database privileges to the SQL user
 mysql -e "GRANT ALL PRIVILEGES ON \`$SQL_DATABASE\`.* TO \`$SQL_USER\`@'%' IDENTIFIED BY '$SQL_PASSWORD';"
