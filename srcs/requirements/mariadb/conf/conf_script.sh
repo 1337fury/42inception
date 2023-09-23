1ac06a6d08161b1e55763fa56260230ff352f7d4
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-23 15:34:42 +0100

    update

--- srcs/requirements/mariadb/conf/conf_script.sh
+++ srcs/requirements/mariadb/conf/conf_script.sh
@@ -1,5 +1,6 @@
-#!/bin/bash
-
-sed -i 's/bind-address\s*=\s*127\.0\.0\.1/bind-address            = 0.0.0.0/' /etc/mysql/mariadb.conf.d/50-server.cnf
-
-sed -i 's/user\s*=\s*mysql/user                    = root/' /etc/mysql/mariadb.conf.d/50-server.cnf
+[mysqld]
+datadir 			= /var/lib/mysql
+socket  			= /run/mysqld/mysqld.sock
+bind_address		= 0.0.0.0
+port 				= 3306
+user 				= mysql
