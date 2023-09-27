e9a8e2c9fb066087cfc22d27ee5650e6c0c2f120
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-27 19:44:08 +0100

    remove comments

--- srcs/requirements/wordPress/tools/config_wp.sh
+++ srcs/requirements/wordPress/tools/config_wp.sh
@@ -1,31 +1,3 @@
-# #!/bin/bash
-
-# # Define configuration variables
-# WP_CONFIG_FILE="./wp-config.php"
-# WORDPRESS_URL="http://wordpress.org/latest.tar.gz"
-
-# # Check if wp-config.php exists
-# if [ -f "$WP_CONFIG_FILE" ]; then
-#     echo "WordPress is already configured."
-# else
-#     sleep 5
-#     # Download and extract WordPress
-#     wget "$WORDPRESS_URL" -O - | tar -xz -C .
-
-#     # Rename and configure wp-config.php
-#     mv wp-config-sample.php wp-config.php
-#     sed -i "s/username_here/$SQL_USER/g" wp-config.php
-#     sed -i "s/password_here/$SQL_PASSWORD/g" wp-config.php
-#     sed -i "s/localhost/$DOMAIN_NAME/g" wp-config.php
-#     sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config.php
-#     echo "WordPress has been configured."
-# fi
-
-# # Execute the provided command (e.g., start a web server)
-# exec "$@"
-
-####################################################################################################
-
 #!/bin/sh
 
 #check if wp-config.php exist
@@ -42,12 +14,6 @@ else
 	rm -rf latest.tar.gz wordpress
 	apt install sendmail -y
 
-	# #Inport env variables in the config file
-	# sed -i "s/username_here/$SQL_USER/g" wp-config-sample.php
-	# sed -i "s/password_here/$SQL_PASSWORD/g" wp-config-sample.php
-	# sed -i "s/localhost/$DB_HOST/g" wp-config-sample.php
-	# sed -i "s/database_name_here/$SQL_DATABASE/g" wp-config-sample.php
-	# cp wp-config-sample.php wp-config.php
 	 wp config create --allow-root \
         --dbname=$SQL_DATABASE \
         --dbuser=$SQL_USER \
