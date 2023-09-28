240b13f66088f22b4bf6f82cf86847839b284861
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-09-28 15:32:42 +0100

    vsftpd

---
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+service vsftpd start
+
+useradd $FTP_USER -d /var/www/wordpress -M --password $FTP_PASS
+echo $FTP_USER >> /etc/vsftpd.user_list
+chown $FTP_USER:$FTP_USER -R /home/$FTP_USER
+
+service vsftpd stop
+/usr/sbin/vsftpd
