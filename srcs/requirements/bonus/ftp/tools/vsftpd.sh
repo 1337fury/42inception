05472a8cf248e130b52f31c84cfeb4dd37f625b9
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-04 11:18:10 +0100

    change creating user pass method

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -2,7 +2,7 @@
 
 service vsftpd start
 
-useradd $FTP_USER -d /home/$FTP_USER -M --password $FTP_PASS
+useradd username -d /home/$FTP_USER && echo "$FTP_USER:$FTP_PASS" | sudo chpasswd
 mkdir -p /home/$FTP_USER/mysite
 echo $FTP_USER > /etc/vsftpd.userlist
 echo $FTP_USER > /etc/vsftpd.chroot_list
