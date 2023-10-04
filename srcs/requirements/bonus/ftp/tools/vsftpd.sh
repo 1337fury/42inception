024e85102fd20995b0cdb7dc07f2c258975a950a
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-04 12:48:05 +0100

    during test

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -7,6 +7,7 @@ mkdir -p /home/$FTP_USER/mysite
 echo $FTP_USER > /etc/vsftpd.userlist
 echo $FTP_USER > /etc/vsftpd.chroot_list
 chown $FTP_USER:$FTP_USER -R /home/$FTP_USER
+echo "$FTP_USER:$FTP_PASS" |  chpasswd
 
 service vsftpd stop
 /usr/sbin/vsftpd
