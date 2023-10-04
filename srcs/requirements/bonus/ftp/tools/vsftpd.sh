8316e17975ba16892656d2085b1596f282725ab2
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-04 12:11:42 +0100

    more test ..

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -3,11 +3,11 @@
 $FTP_USER=fury
 $FTP_PASS=1234
 
-service vsftpd start
-
 useradd $FTP_USER -d /home/$FTP_USER
 echo "$FTP_USER:$FTP_PASS" | sudo chpasswd
-# useradd fury -d /home/fury
+
+service vsftpd start
+
 mkdir -p /home/$FTP_USER/mysite
 echo $FTP_USER > /etc/vsftpd.userlist
 echo $FTP_USER > /etc/vsftpd.chroot_list
