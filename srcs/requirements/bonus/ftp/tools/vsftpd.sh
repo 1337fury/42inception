27c7e5ea7954e9e2d0e3e79d11e9a6cc92c2c09b
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-04 11:24:32 +0100

    fix missing user name

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -2,7 +2,7 @@
 
 service vsftpd start
 
-useradd username -d /home/$FTP_USER && echo "$FTP_USER:$FTP_PASS" | sudo chpasswd
+useradd $FTP_USER -d /home/$FTP_USER && echo "$FTP_USER:$FTP_PASS" | sudo chpasswd
 mkdir -p /home/$FTP_USER/mysite
 echo $FTP_USER > /etc/vsftpd.userlist
 echo $FTP_USER > /etc/vsftpd.chroot_list
