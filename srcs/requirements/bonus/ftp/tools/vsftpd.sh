307cc87630223655e4feeb8ce88ecb19392d8a9a
Abdelhalim El Ouardy <abdeel-o@c2r1p9.1337.ma>
2023-10-04 12:58:27 +0100

    does not work

--- srcs/requirements/bonus/ftp/tools/vsftpd.sh
+++ srcs/requirements/bonus/ftp/tools/vsftpd.sh
@@ -2,11 +2,12 @@
 
 service vsftpd start
 
+useradd $FTP_USER -d /home/$FTP_USER -M --password $FTP_PASS
 mkdir -p /home/$FTP_USER/mysite
 echo $FTP_USER > /etc/vsftpd.userlist
 echo $FTP_USER > /etc/vsftpd.chroot_list
 chown $FTP_USER:$FTP_USER -R /home/$FTP_USER
-echo "$FTP_USER:$FTP_PASS" | chpasswd
+echo "$FTP_USER:$FTP_PASS" |  chpasswd
 
 service vsftpd stop
 /usr/sbin/vsftpd
